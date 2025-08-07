06186273e98502c2daf08d7bfd25b961a606edbc
krish406 <155498135+krish406@users.noreply.github.com>
2025-08-06 20:27:02 -0400

    setup initial layout

--- weather-site-vite/src/App.tsx
+++ weather-site-vite/src/App.tsx
@@ -1,33 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function Form(){
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <form className='flex flex-row gap-2'>
+        <input type="text" name="location" id="location" size = {50} placeholder='Enter a location here'/>
+        <button type = "submit" className='bg-blue-500 hover:bg-blue-700 text-white py-1 px-2 border border-blue-700 rounded'>Submit</button>
+      </form>
+    </>
+  )
+}
+
+function App() {
+  return (
+    <>
+      <div className = "flex flex-col m-5 justify-center items-center">
+        <p className='text-center m-2'>Weather App</p>
+        <Form></Form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
